414d328f6148078aa3f7ce0b37ab6fb279f04c1f
Quinlan Pfiffer <shithouse@goatse.cx>
2013-07-16 20:33:12 -0700

    BEAUTIFUL COLORS

--- src/merveilles_io/main.py
+++ src/merveilles_io/main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
-import kyotocabinet
+from kyotocabinet import DB
 
 app = Flask(__name__)
+
+def get_links(num):
+    db = DB()
+    if not db.open("links.kct", DB.OREADER | DB.OCREATE):
+        print "Could not open database."
+    return db[:num]
 
 @app.route("/")
 def root():
     items = [{"person": "person{0}".format(i),
+              "title": "Killing Ducks: A mortuary.",
               "link":"http://link{0}.com".format(i)} for i in range(0,25)]
     return render_template("index.html", items=items)
 
