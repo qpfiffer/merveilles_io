a09b3387237511eae7414c99bd19b4c0b63ebfc0
Quinlan Pfiffer <shithouse@goatse.cx>
2013-07-16 19:36:00 -0700

    setuptools sucks

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.1'
+
+setup(name='merveilles_io',
+      version=version,
+      author='Quinlan Pfiffer',
+      author_email='qpfiffer@gmail.com',
+      license='LICENSE',
+      packages=["merveilles_io"],
+      package_dir = {'':'src'},
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'flask',
+      ],
+      entry_points={
+          'console_scripts': [
+              'merveilles_io = merveilles_io.main:main',
+          ]
+      },
+      )
---
+++ src/merveilles_io/main.py
@@ -0,0 +1,4 @@
+import kyotocabinet
+
+def main():
+    print "Hello, World!"
