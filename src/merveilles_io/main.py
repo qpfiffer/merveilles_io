9fcdb1cc98e1d9396113c98dae36c5d420864a58
Quinlan Pfiffer <qpfiffer@gmail.com>
2013-07-24 12:17:54 -0700

    Added file size extension thing.

--- src/merveilles_io/main.py
+++ src/merveilles_io/main.py
@@ -73,6 +73,21 @@ def db_meta_info():
 
     return meta
 
+@app.template_filter('file_size')
+def file_size(size_str):
+    # Found here: http://djangosnippets.org/snippets/1866/
+    value = int(size_str)
+    if value < 512000:
+        value = value / 1024.0
+        ext = 'kb'
+    elif value < 4194304000:
+        value = value / 1048576.0
+        ext = 'mb'
+    else:
+        value = value / 1073741824.0
+        ext = 'gb'
+    return '%s %s' % (str(round(value, 2)), ext)
+
 @app.template_filter('unix_to_human')
 def unix_to_human(timestamp_str):
     time = float(timestamp_str)
