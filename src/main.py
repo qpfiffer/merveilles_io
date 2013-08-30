e776510e1acae56a1c687736450ef8462a69e901
Quinlan Pfiffer <shithouse@goatse.cx>
2013-08-30 01:26:57 -0700

    Forgot this in the refactor.

--- src/main.py
+++ src/main.py
@@ -135,7 +135,7 @@ def db_meta_info():
     meta = {}
     db = DB()
     db_file = app.config['DB_FILE']
-    if not db.open("{0}links.kct".format(db_file), DB.OREADER):
+    if not db.open("{0}".format(db_file), DB.OREADER):
         print "Could not open database."
     meta["size"] = db.size()
     meta["count"] = db.count()
