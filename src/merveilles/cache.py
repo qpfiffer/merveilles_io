a3f2482a9dc2ce523122d0deecfdf47bead64148
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-03-08 12:47:44 -0800

    Compression, expiration and better keys for the OlegDB cache.

--- src/merveilles/cache.py
+++ src/merveilles/cache.py
@@ -2,7 +2,7 @@ from context_processors import db_meta_info
 from flask import request, current_app
 from functools import wraps
 from kyotocabinet import DB
-import requests
+import requests, time, zlib, urllib
 
 def ol_view_cache(f):
     @wraps(f)
@@ -12,15 +12,27 @@ def ol_view_cache(f):
             return f(*args, **kwargs)
 
         res = None
-        fancy = hash("{}{}{}".format(db_meta_info()['count'], request.url, f.func_name))
+        fancy = "{}{}{}{}".format(db_meta_info()['count'],
+                request.host,
+                request.query_string,
+                f.func_name)
+        quoted = urllib.quote(fancy.encode('ascii', 'replace'))
 
-        resp = requests.get("http://localhost:8080/{}".format(fancy))
+        resp = requests.get("http://localhost:8080/{}".format(quoted), stream=True)
         if resp.status_code == 404:
             res = f(*args, **kwargs)
-            requests.post("http://localhost:8080/{}".format(fancy),
-                data=res.encode('utf-8'), headers={"Content-Type": "text/html"})
+            # 24 hours
+            expiration = int(time.mktime(time.gmtime())) + (60 * 60 * 24)
+            utf_data = res.encode('utf-8')
+            compressed = zlib.compress(utf_data)
+            requests.post("http://localhost:8080/{}".format(quoted),
+                data=compressed,
+                headers={
+                    "Content-Type": "text/html",
+                    "X-OlegDB-use-by": expiration})
         else:
-            res = resp.text
+            compressed = resp.raw.read()
+            res = zlib.decompress(compressed)
 
         return res
     return decorated_function
