751159f5fa9d203172dc36331454dfc153f5274f
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-05-26 13:44:46 -0700

    Hide login icon if you're logged in.

--- src/merveilles/context_processors.py
+++ src/merveilles/context_processors.py
@@ -1,5 +1,5 @@
 from kyotocabinet import DB
-from flask import current_app, Blueprint
+from flask import current_app, Blueprint, g, session
 
 app = Blueprint("wut", __name__, template_folder='templates')
 
@@ -8,6 +8,18 @@ def site_settings():
     return {"live_site": current_app.config['LIVE_SITE'],
             "channel": current_app.config['CHANNEL']
             }
+
+@app.app_context_processor
+def get_user():
+    from merveilles.database import _get_user_str
+    user = None
+    username = session.get("username")
+    if username:
+        oleg = g.oleg
+        user_str = _get_user_str(username)
+        user = oleg.get(user_str)
+
+    return {"user": user}
 
 @app.app_context_processor
 def db_meta_info():
